d9ebb781c266b7b9adddde79140d7cdb25f5e808
Kristian Gaylord <krlgaylord@gmail.com>
2024-03-17 19:11:59 -0400

    interactive

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
 export $(cat .env | xargs)
 
-source .venv/bin/activate && python armor.py "$@"
+# Check if the first argument is "interactive"
+if [ "$1" = "interactive" ]; then
+    source .venv/bin/activate && python interactive.py
+else
+    source .venv/bin/activate && python armor.py "$@"
+fi
+
